91d72e4254cfb44ddfb303900fc13e1f70041021
Mads Holten Rasmussen <mads@mads.lan>
2024-03-20 13:26:27 +0100

    Added tests + query definition support + bumped to new version of oxigraph

--- library/src/index.ts
+++ library/src/index.ts
@@ -1,4 +1,4 @@
-import { TaskType, WorkerInstruction, WorkerResult, ResponseMimetype, RDFMimetype } from "./interfaces";
+import { TaskType, WorkerInstruction, WorkerResult, ResponseMimetype, RDFMimetype, QueryDefinition, TemplateData } from "./interfaces";
 
 /**
  * Async Oxigraph is a wrapper on top of Oxigraph that allows you to run an oxigraph instance 
@@ -12,10 +12,20 @@ export * from './interfaces';
 
 export class AsyncOxigraph{
 
-    private worker: Worker;
+    private static _instance: AsyncOxigraph;
+    private _worker: Worker;
+    private _queryDefs: QueryDefinition[] = [];
 
-    constructor(workerPath: string = "./assets/oxigraph/worker.js"){
-        this.worker = new Worker(workerPath);
+    private constructor(workerPath: string){
+        this._worker = new Worker(workerPath);
+    }
+
+    static getInstance(workerPath: string = "./assets/oxigraph/worker.js") {
+        if (this._instance) {
+            return this._instance;
+        }
+        this._instance = new AsyncOxigraph(workerPath);
+        return this._instance;
     }
 
     /**
@@ -24,7 +34,7 @@ export class AsyncOxigraph{
      * @returns a worker result with a message
      */
     async init(wasmPath: string = "./web_bg.wasm"): Promise<WorkerResult>{
-        return await this.runBackgroundTask({task: TaskType.INIT, initPayload: {wasmPath}});
+        return await this._runBackgroundTask({task: TaskType.INIT, initPayload: {wasmPath}});
     }
 
     /**
@@ -36,7 +46,7 @@ export class AsyncOxigraph{
      * @returns 
      */
     async load(triples: string, mimetype: RDFMimetype, baseURI?: string, graphURI?: string): Promise<WorkerResult>{
-        return await this.runBackgroundTask({task: TaskType.LOAD, loadPayload: {triples, mimetype, baseURI, graphURI}});
+        return await this._runBackgroundTask({task: TaskType.LOAD, loadPayload: {triples, mimetype, baseURI, graphURI}});
     }
 
     /**
@@ -46,7 +56,7 @@ export class AsyncOxigraph{
      * @returns 
      */
     async query(query: string, responseMimetype?: ResponseMimetype): Promise<WorkerResult>{
-        return await this.runBackgroundTask({task: TaskType.QUERY, queryPayload: {query, responseMimetype}});
+        return await this._runBackgroundTask({task: TaskType.QUERY, queryPayload: {query, responseMimetype}});
     }
 
     /**
@@ -56,25 +66,87 @@ export class AsyncOxigraph{
      * @returns 
      */
     async dump(mimetype: RDFMimetype = RDFMimetype.NQUADS, graphURI?: string): Promise<WorkerResult>{
-        return await this.runBackgroundTask({task: TaskType.DUMP, dumpPayload: {mimetype, graphURI}});
+        return await this._runBackgroundTask({task: TaskType.DUMP, dumpPayload: {mimetype, graphURI}});
+    }
+
+    /**
+     * Get store size
+     * @returns the size as a number
+     */
+    async size(): Promise<number>{
+        const query = "SELECT (COUNT(*) AS ?count) WHERE {?s ?p ?o}";
+        const res = await this.query(query);
+        return parseInt(res.data.results.bindings[0].count.value);
+    }
+
+    /**
+     * Add a query definition that can easily be executed
+     * @param queryDefs array of query definitions 
+     */
+    addQueryDefinitions(queryDefs: QueryDefinition[] = []): void{
+        queryDefs.forEach(query => {
+          const match = this._queryDefs.find(q => q.id === query.id);
+          if(match === undefined) this._queryDefs.push(query);
+        });
+    }
+
+    /**
+     * Execute a saved query
+     * @param queryId the id of the query
+     * @param params optional query params to replace in the query
+     */
+    async execute(queryId: string, params?: TemplateData): Promise<WorkerResult>{
+        const queryData = this._queryDefs.find(q => q.id === queryId);
+        if(queryData === undefined) throw new Error(`Unknown query with id "${queryId}"`);
+
+        const query = this._replaceHandlebars(queryData.query, params);
+
+        const res = await this.query(query, queryData.responseMimetype);
+        res.query = query;
+        
+        if(queryData.postProcessing !== undefined){
+            const initialTime = this._getPostProcessingTime(res.message);
+            const t1 = new Date().getTime();
+            res.data = await queryData.postProcessing(res.data);
+            const t2 = new Date().getTime();
+            const dt = (t2-t1)/1000;
+            const total = initialTime+dt;
+            res.message = res.message.replace(initialTime.toString(), total.toString());
+        }
+
+        return res;
     }
 
     close(){
-        this.worker.terminate();
+        this._worker.terminate();
     }
 
-    private async runBackgroundTask(instruction: WorkerInstruction): Promise<WorkerResult>{
+    private async _runBackgroundTask(instruction: WorkerInstruction): Promise<WorkerResult>{
         return new Promise((resolve, reject) => {
 
             // Resolve result
-            this.worker.addEventListener('message', (event) => {
+            this._worker.addEventListener('message', (event) => {
                 if(event.data.error != undefined) reject(event.data.error);
                 resolve(event.data);
             }, false);
 
-            this.worker.postMessage(instruction); // Send data to our worker.
+            this._worker.postMessage(instruction); // Send data to our worker.
 
         })
+    }
+
+    private _replaceHandlebars(template: string, data: TemplateData): string {
+        const regex = /{{(.*?)}}/g; // Regex to find {{variable}} patterns
+      
+        return template.replace(regex, (match, variableName) => {
+          return data.hasOwnProperty(variableName) ? data[variableName].toString() : '';
+          // If the variable exists in the data, replace it; otherwise, replace with an empty string
+        });
+    }
+
+    private _getPostProcessingTime(text: string): number{
+        const match = text.match(/(\d+\.\d+)\s*seconds/);
+        return match ? parseFloat(match[1]) : 0;
     }
 
 }
